80f24c4cc1c317f6ab2839d5917958115cb430ca
anjalii0701 <anjalisharma288863@gmail.com>
2025-09-14 16:06:24 +0530

    created a express server

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const  express =require("express");
+
+const app=express();
+
+app.use("/hello",(req,res)=>{
+    res.send("hello from server ")
+});
+
+app.use("/test",(req,res)=>{
+    res.send("hello test from server")
+});
+
+app.listen(3000 ,()=>{
+    console.log("server is successfully listening")
+});
